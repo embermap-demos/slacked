5d37f2a406e98c74f787c33c25c2d311acd4fa96
Sam Selikoff <sam.selikoff@gmail.com>
2018-06-13 15:22:05 -0400

    End of video 2

---
+++ app/tailwind/config/colors.js
@@ -0,0 +1,32 @@
+/*
+|-------------------------------------------------------------------------------
+| Colors                                    https://tailwindcss.com/docs/colors
+|-------------------------------------------------------------------------------
+|
+| Here you can specify the colors used in your project. To get you started,
+| we've provided a generous palette of great looking colors that are perfect
+| for prototyping, but don't hesitate to change them for your project. You
+| own these colors, nothing will break if you change everything about them.
+|
+| We've used literal color names ("red", "blue", etc.) for the default
+| palette, but if you'd rather use functional names like "primary" and
+| "secondary", or even a numeric scale like "100" and "200", go for it.
+|
+*/
+
+export default {
+  transparent: 'transparent',
+
+  black: '#22292f',
+  'grey-darkest': '#3d4852',
+  'grey-darker': '#606f7b',
+  'grey-dark': '#8795a1',
+  grey: '#b8c2cc',
+  'grey-light': '#dae1e7',
+  'grey-lighter': '#f1f5f8',
+  'grey-lightest': '#f8fafc',
+  white: '#ffffff',
+
+  'purple': '#4D394B',
+  'purple-dark': '#271D26'
+};
