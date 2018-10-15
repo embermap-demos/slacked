2fa5107a265ff57c8bc2c61e876389e9ed828d31
Sam Selikoff <sam.selikoff@gmail.com>
2018-10-15 01:56:08 -0400

    End of Episode 14

---
+++ app/pods/components/ui-button/component.js
@@ -0,0 +1,13 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  tagName: '',
+
+
+  // Single style option
+  style: 'regular white'
+
+  // Per-argument option
+  // size: 'regular',
+  // color: 'white'
+});
